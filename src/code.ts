66e61580aee456d0df40f5a833cb335530283356
David Asensio Cañas <david.asensio@icloud.com>
2022-01-15 16:44:07 +0100

    Add webpack configuration

---
+++ src/code.ts
@@ -0,0 +1,35 @@
+init()
+
+function init () {
+    figma.showUI(__html__);
+    sendSelectedTextNodeDataToUI()
+
+    figma.ui.onmessage = handleUIMessage
+}
+
+function sendSelectedTextNodeDataToUI () {
+    const selectedTextNode = getSelectedTextNode()
+
+    const { id, characters } = selectedTextNode
+    figma.ui.postMessage({
+        type: 'text-selected',
+        payload: { id, characters }
+    })
+}
+
+function getSelectedTextNode () {
+    const { selection } = figma.currentPage
+    const selectedTextNode = selection.find(
+        ({ type }) => type === 'TEXT'
+    )
+
+    return selectedTextNode as TextNode
+}
+
+function handleUIMessage(msg) {
+    if (msg.type === 'create-copy') {
+        console.log('Creating copy', msg.payload)
+    }
+
+    figma.closePlugin();
+}
---
+++ src/ui.ts
@@ -0,0 +1,34 @@
+onmessage = handleMessage
+
+const tagInputEl = $('tag-input') as HTMLInputElement
+const englishInputEl = $('english-input') as HTMLInputElement
+const saveButtonEl = $('save-button') as HTMLButtonElement
+
+saveButtonEl.onclick = handleSaveClick
+
+function handleSaveClick () {
+    sendMessage({
+        type: 'create-copy',
+        payload: {
+            tag: tagInputEl.value,
+            languages: {
+                en: englishInputEl.value
+            }
+        }
+    })
+}
+
+function handleMessage ({ data }) {
+    const { type, payload } = data.pluginMessage
+    if (type === 'text-selected') {
+        englishInputEl.value = payload.characters
+    }
+}
+
+function sendMessage(message) {
+    parent.postMessage({pluginMessage: message }, '*')
+}
+
+function $(elementId) {
+    return document.getElementById(elementId)
+}
